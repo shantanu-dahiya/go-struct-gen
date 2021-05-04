68968ff598efde7fbb32966136c8c26069c87af1
Shantanu Dahiya <shantanu.webmail@gmail.com>
2021-05-05 01:19:14 +0530

    app port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from flask import Flask, render_template, url_for, request
 from generator import generate
 
@@ -16,4 +17,5 @@ def my_form_post():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
